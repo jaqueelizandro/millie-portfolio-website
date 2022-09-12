18b3e89a5d3b1aa122087e057eff7997a5dee0c8
Jaque Elizandro <jaque@elizandro.com.br>
2022-09-12 13:21:32 +1000

    change function carousel to acept more slides

--- js/parametric-insurance.js
+++ js/parametric-insurance.js
@@ -1,25 +1,31 @@
 const nextButton = document.querySelector(".button-right");
 const prevButton = document.querySelector(".button-left");
-const slides = document.querySelector(".slides");
-const firstSlide = slides.querySelector("#slide-1");
-const secondSlide = slides.querySelector("#slide-2");
 
-nextButton.addEventListener('click', e => {
-    const currentSlide = slides.querySelector(".current-slide");
-    if (currentSlide === firstSlide) {
-        currentSlide.classList.remove("current-slide");
-        secondSlide.classList.add("current-slide");
-        prevButton.classList.remove("button-inactive");
-        nextButton.classList.add("button-inactive")
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }    
+    if (n < 1) {
+        slideIndex = slides.length;
     }
-})
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
 
 prevButton.addEventListener('click', e => {
-    const currentSlide = slides.querySelector(".current-slide");
-    if (currentSlide === secondSlide) {
-        currentSlide.classList.remove("current-slide");
-        firstSlide.classList.add("current-slide");
-        prevButton.classList.add("button-inactive");
-        nextButton.classList.remove("button-inactive")
-    }
+    plusSlides(-1)
+})
+
+nextButton.addEventListener('click', e => {
+    plusSlides(1)
 })
