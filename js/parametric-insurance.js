f1a0152e885005b76f1332b57ddbc488e695d65f
Jaque Elizandro <jaque@elizandro.com.br>
2022-09-13 10:57:36 +1000

    carousel ideation section working

--- js/parametric-insurance.js
+++ js/parametric-insurance.js
@@ -1,5 +1,5 @@
-const nextButton = document.querySelector(".button-right");
 const prevButton = document.querySelector(".button-left");
+const nextButton = document.querySelector(".button-right");
 let slides = document.getElementsByClassName("slide");
 
 let slideIndex = 1;
@@ -8,17 +8,17 @@ prevButton.addEventListener('click', e => {
     if (slideIndex !== 1) {
         plusSlides(-1);
     }
-})
+});
 
 nextButton.addEventListener('click', e => {
     if (slideIndex !== slides.length) {
         plusSlides(1);
     }
-})
+});
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    showSlides(slideIndex += n);
+};
 
 function showSlides(n) {
     if (n > slides.length) {
@@ -42,3 +42,49 @@ function showSlides(n) {
     }
     
 }
+
+//////////////////////////////////////
+
+const ideationPrevButton = document.querySelector(".ideation__button-left");
+const ideationNextButton = document.querySelector(".ideation__button-right");
+let ideationSlides = document.getElementsByClassName("ideation__slide");
+
+let ideationSlideIndex = 1;
+
+ideationPrevButton.addEventListener('click', e => {
+    if (ideationSlideIndex !== 1) {
+        ideationPlusSlides(-1);
+    }
+});
+
+ideationNextButton.addEventListener('click', e => {
+    if (ideationSlideIndex !== ideationSlides.length) {
+        ideationPlusSlides(1);
+    }
+});
+
+function ideationPlusSlides(n) {
+    showSlidesIdeation(ideationSlideIndex += n);
+};
+
+function showSlidesIdeation(n) {
+    if (n > ideationSlides.length) {
+        ideationSlideIndex = 1;
+    }    
+    if (n < 1) {
+        ideationSlideIndex = ideationSlides.length;
+    }
+    for (let i = 0; i < ideationSlides.length; i++) {
+        ideationSlides[i].classList.remove("current-slide")
+    }
+    ideationSlides[ideationSlideIndex-1].classList.add('current-slide');
+
+    if (ideationSlideIndex === ideationSlides.length) {
+        ideationNextButton.classList.add('button-inactive');
+        ideationPrevButton.classList.remove('button-inactive');
+    }
+    if (ideationSlideIndex === 1) {
+        ideationPrevButton.classList.add('button-inactive');
+        ideationNextButton.classList.remove('button-inactive');
+    }
+};
