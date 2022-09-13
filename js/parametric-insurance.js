acff709e13f262d6229da9f86cbc8ad0b305f61c
Jaque Elizandro <jaque@elizandro.com.br>
2022-09-13 13:28:36 +1000

    add function to doesnt show the nav bar when the back to the top button appears at the screen

--- js/parametric-insurance.js
+++ js/parametric-insurance.js
@@ -88,3 +88,16 @@ function showSlidesIdeation(n) {
         ideationNextButton.classList.remove('button-inactive');
     }
 };
+
+//////////////////////////////////////
+
+const observer = new IntersectionObserver(function(entries) {
+    const nav = document.querySelector("header");
+	if(entries[0].isIntersecting === true) {
+        nav.classList.add("invisible")
+    } else {
+        nav.classList.remove("invisible")
+    }
+}, { threshold: [1] });
+
+observer.observe(document.querySelector("#back-top-button"));
